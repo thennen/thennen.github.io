2403f3186b5ed0e8d2b036925b8e3c0c934d01b5
Tyler Hennen <tyler@hennen.us>
2025-07-16 15:21:15 +0200

    Fix page title

--- generate_publications.py
+++ generate_publications.py
@@ -17,8 +17,7 @@ header = """
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Publications</title>
-    <title>Tyler Hennen, PhD</title>
+    <title>Tyler Hennen's Publications</title>
     <link rel="stylesheet" href="cardstyles.css">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
